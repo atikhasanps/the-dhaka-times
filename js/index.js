3490cc6b301830753cb8c3cd025deebbb913a7c5
atikhasanps <atikhasanps@gmail.com>
2022-09-03 15:36:14 +0600

    second commit

--- js/index.js
+++ js/index.js
@@ -13,10 +13,40 @@ const setAllMenu = async()=>{
     const data = await loadAllNews();
     
     for(const news of data){
-        console.log(news.category_name)
+        // console.log(news.category_name)
     }
     
 }
 setAllMenu();
 
-loadAllNews();
+// loadAllNews();
+
+
+const btnHome = document.getElementById('btn-home');
+
+btnHome.addEventListener('click', async(event)=>{
+    const allNews = await loadAllNews();
+    // console.log(allNews)
+    const newsContainer = document.getElementById('news-container');
+    newsContainer.textContent = ""; 
+
+    allNews.forEach(news => {
+        // console.log(news)
+
+        const div = document.createElement("div");
+        div.innerHTML =`
+        <div class="card lg:card-side bg-base-100 shadow-xl">
+        <figure><img src="https://placeimg.com/400/400/arch" alt="Album"></figure>
+        <div class="card-body">
+          <h2 class="card-title">New album is released!</h2>
+          <p>Click the button to listen on Spotiwhy app.</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Listen</button>
+          </div>
+        </div>
+      </div>
+        `
+        newsContainer.appendChild(div)
+    });
+    
+})
