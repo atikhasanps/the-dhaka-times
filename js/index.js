4d8c25bf1f961528e57a050bd8ed01001f82ac98
atikhasanps <atikhasanps@gmail.com>
2022-09-04 22:05:05 +0600

    third commit

--- js/index.js
+++ js/index.js
@@ -1,52 +1,68 @@
-// const loadAllNews = async()=>{
-//     const response = await fetch("https://openapi.programming-hero.com/api/news/categories");
+const loadAllNews = async()=>{
+    const response = await fetch("https://openapi.programming-hero.com/api/news/category/01");
     
-//     const data = await response.json();
-//     const getData = data.data.news_category ;
-//     return getData ;
+    const data = await response.json();
+    const getData = data.data ;
     
-//     // console.log(data.data.news_category)
-// }
-
-// const setAllMenu = async()=>{
-
-//     const data = await loadAllNews();
+    displayNews(getData);
     
-//     for(const news of data){
-//         // console.log(news.category_name)
-//     }
     
-// }
-// setAllMenu();
+  
+}
+const displayNews = allnews =>{
+    const newsContainer = document.getElementById('news-container');
+    allnews.forEach(news => {
+        const newsDiv = document.createElement('div');
+        newsDiv.classList.add('col')
+        newsDiv.innerHTML =`
+        <div class="card lg:card-side bg-base-100 shadow-xl">
+         <figure><img src="${news.thumbnail_url}" alt="Album"></figure>
+         <div class="card-body">
+          <h2 class="card-title">${news.title}</h2>
+         <p>${news.details.slice(0,350)}</p>
+           <div class="card-actions justify-between">
+           <div class="grid grid-cols-2 ">
+           <img class="w-8 rounded-full"  src="${news.author.img}">
+           <h2>${news.author.name}</h2>
+           </div>
+           <div>
+           <h1 class="text-xl"><i class="fa-sharp fa-solid fa-eye"></i>${news.total_view}</h1>
+           </div>
+           <div>
+           <label for="my-modal-6" onclick="showModal()" class="btn modal-button">Show Details</label>
+           </div>
+             
+           </div>
+         </div>
+       </div>
+         `;
+        newsContainer.appendChild(newsDiv)
+    });
+}
 
-// // loadAllNews();
 
 
-// const btnHome = document.getElementById('btn-home');
 
-// btnHome.addEventListener('click', async(event)=>{
-//     const allNews = await loadAllNews();
-//     // console.log(allNews)
-//     const newsContainer = document.getElementById('news-container');
-//     newsContainer.textContent = ""; 
+const showModal =()=>{
+    const modalBody = document.getElementById('modal-body');
+    modalBody.innerHTML =`
+    <h3 class="font-bold text-lg">${news.title}</h3>
+    <img src="">
+    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+    <div class="modal-action">
+      <label for="modal-body" class="btn">Yay!</label>
+    </div>
 
-//     allNews.forEach(news => {
-//         // console.log(news)
-
-//         const div = document.createElement("div");
-//         div.innerHTML =`
-//         <div class="card lg:card-side bg-base-100 shadow-xl">
-//         <figure><img src="${news.thumbnail_url}" alt="Album"></figure>
-//         <div class="card-body">
-//           <h2 class="card-title">New album is released!</h2>
-//           <p>Click the button to listen on Spotiwhy app.</p>
-//           <div class="card-actions justify-end">
-//             <button class="btn btn-primary">Listen</button>
-//           </div>
-//         </div>
-//       </div>
-//         `
-//         newsContainer.appendChild(div)
-//     });
+    `;
     
-// })
+}
+
+document.getElementById('btn-news').addEventListener('click', function(){
+    
+  
+        
+   
+})
+
+
+loadAllNews();
--- js/test.js
+++ js/test.js
@@ -1,51 +1,70 @@
-// const loadNews = () =>{
-//     fetch('https://openapi.programming-hero.com/api/news/categories')
-//     .then(res => res.json())
-//     .then(data => console.log(data.data.news_category))
-// }
 
-// loadNews();
 
-const loadAllNews = async()=>{
-    const response = await fetch("https://openapi.programming-hero.com/api/news/category/01");
+// const loadAllNews = async()=>{
+//     const response = await fetch("https://openapi.programming-hero.com/api/news/category/01");
     
-    const data = await response.json();
-    const getData = data.data ;
+//     const data = await response.json();
+//     const getData = data.data ;
     
-    displayNews(getData);
+//     displayNews(getData);
     
     
   
-}
-const displayNews = allnews =>{
-    const newsContainer = document.getElementById('news-container');
-    allnews.forEach(news => {
-        const newsDiv = document.createElement('div');
-        newsDiv.classList.add('col')
-        newsDiv.innerHTML =`
-        <div class="card lg:card-side bg-base-100 shadow-xl">
-         <figure><img src="${news.thumbnail_url}" alt="Album"></figure>
-         <div class="card-body">
-          <h2 class="card-title">${news.title}</h2>
-         <p>${news.details}</p>
-           <div class="card-actions justify-between">
-           <div class="grid grid-cols-2">
-           <img class="w-8 rounded-full"  src="${news.author.img}">
-           <h2>${news.author.name}</h2>
-           </div>
-           <div>
-           <h1 class="text-xl"><i class="fa-sharp fa-solid fa-eye"></i>${news.total_view}</h1>
-           </div>
-           <div>
-           <button class="btn btn-primary">Listen</button>
-           </div>
+// }
+// const displayNews = allnews =>{
+//     const newsContainer = document.getElementById('news-container');
+//     allnews.forEach(news => {
+//         const newsDiv = document.createElement('div');
+//         newsDiv.classList.add('col')
+//         newsDiv.innerHTML =`
+//         <div class="card lg:card-side bg-base-100 shadow-xl">
+//          <figure><img src="${news.thumbnail_url}" alt="Album"></figure>
+//          <div class="card-body">
+//           <h2 class="card-title">${news.title}</h2>
+//          <p>${news.details.slice(0,350)}</p>
+//            <div class="card-actions justify-between">
+//            <div class="grid grid-cols-2 ">
+//            <img class="w-8 rounded-full"  src="${news.author.img}">
+//            <h2>${news.author.name}</h2>
+//            </div>
+//            <div>
+//            <h1 class="text-xl"><i class="fa-sharp fa-solid fa-eye"></i>${news.total_view}</h1>
+//            </div>
+//            <div>
+//            <label for="my-modal-6" onclick="showModal()" class="btn modal-button">Show Details</label>
+//            </div>
              
-           </div>
-         </div>
-       </div>
-         `;
-        newsContainer.appendChild(newsDiv)
-    });
-}
+//            </div>
+//          </div>
+//        </div>
+//          `;
+//         newsContainer.appendChild(newsDiv)
+//     });
+// }
 
-loadAllNews();
+
+
+
+// const showModal =()=>{
+//     const modalBody = document.getElementById('modal-body');
+//     modalBody.innerHTML =`
+//     <h3 class="font-bold text-lg">${news.title}</h3>
+//     <img src="">
+//     <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+//     <div class="modal-action">
+//       <label for="modal-body" class="btn">Yay!</label>
+//     </div>
+
+//     `;
+    
+// }
+
+// document.getElementById('btn-news').addEventListener('click', function(){
+    
+  
+        
+   
+// })
+
+
+// loadAllNews();
