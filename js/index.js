1a72d667f3724d3808ee22b64b0874ffca877e2a
atikhasanps <atikhasanps@gmail.com>
2022-09-04 16:43:45 +0600

    third check

--- js/index.js
+++ js/index.js
@@ -1,52 +1,52 @@
-const loadAllNews = async()=>{
-    const response = await fetch("https://openapi.programming-hero.com/api/news/categories");
+// const loadAllNews = async()=>{
+//     const response = await fetch("https://openapi.programming-hero.com/api/news/categories");
     
-    const data = await response.json();
-    const getData = data.data.news_category ;
-    return getData ;
+//     const data = await response.json();
+//     const getData = data.data.news_category ;
+//     return getData ;
     
-    // console.log(data.data.news_category)
-}
+//     // console.log(data.data.news_category)
+// }
 
-const setAllMenu = async()=>{
+// const setAllMenu = async()=>{
 
-    const data = await loadAllNews();
+//     const data = await loadAllNews();
     
-    for(const news of data){
-        // console.log(news.category_name)
-    }
+//     for(const news of data){
+//         // console.log(news.category_name)
+//     }
     
-}
-setAllMenu();
+// }
+// setAllMenu();
 
-// loadAllNews();
+// // loadAllNews();
 
 
-const btnHome = document.getElementById('btn-home');
+// const btnHome = document.getElementById('btn-home');
 
-btnHome.addEventListener('click', async(event)=>{
-    const allNews = await loadAllNews();
-    // console.log(allNews)
-    const newsContainer = document.getElementById('news-container');
-    newsContainer.textContent = ""; 
+// btnHome.addEventListener('click', async(event)=>{
+//     const allNews = await loadAllNews();
+//     // console.log(allNews)
+//     const newsContainer = document.getElementById('news-container');
+//     newsContainer.textContent = ""; 
 
-    allNews.forEach(news => {
-        // console.log(news)
+//     allNews.forEach(news => {
+//         // console.log(news)
 
-        const div = document.createElement("div");
-        div.innerHTML =`
-        <div class="card lg:card-side bg-base-100 shadow-xl">
-        <figure><img src="https://placeimg.com/400/400/arch" alt="Album"></figure>
-        <div class="card-body">
-          <h2 class="card-title">New album is released!</h2>
-          <p>Click the button to listen on Spotiwhy app.</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Listen</button>
-          </div>
-        </div>
-      </div>
-        `
-        newsContainer.appendChild(div)
-    });
+//         const div = document.createElement("div");
+//         div.innerHTML =`
+//         <div class="card lg:card-side bg-base-100 shadow-xl">
+//         <figure><img src="${news.thumbnail_url}" alt="Album"></figure>
+//         <div class="card-body">
+//           <h2 class="card-title">New album is released!</h2>
+//           <p>Click the button to listen on Spotiwhy app.</p>
+//           <div class="card-actions justify-end">
+//             <button class="btn btn-primary">Listen</button>
+//           </div>
+//         </div>
+//       </div>
+//         `
+//         newsContainer.appendChild(div)
+//     });
     
-})
+// })
--- js/test.js
+++ js/test.js
@@ -5,3 +5,47 @@
 // }
 
 // loadNews();
+
+const loadAllNews = async()=>{
+    const response = await fetch("https://openapi.programming-hero.com/api/news/category/01");
+    
+    const data = await response.json();
+    const getData = data.data ;
+    
+    displayNews(getData);
+    
+    
+  
+}
+const displayNews = allnews =>{
+    const newsContainer = document.getElementById('news-container');
+    allnews.forEach(news => {
+        const newsDiv = document.createElement('div');
+        newsDiv.classList.add('col')
+        newsDiv.innerHTML =`
+        <div class="card lg:card-side bg-base-100 shadow-xl">
+         <figure><img src="${news.thumbnail_url}" alt="Album"></figure>
+         <div class="card-body">
+          <h2 class="card-title">${news.title}</h2>
+         <p>${news.details}</p>
+           <div class="card-actions justify-between">
+           <div class="grid grid-cols-2">
+           <img class="w-8 rounded-full"  src="${news.author.img}">
+           <h2>${news.author.name}</h2>
+           </div>
+           <div>
+           <h1 class="text-xl"><i class="fa-sharp fa-solid fa-eye"></i>${news.total_view}</h1>
+           </div>
+           <div>
+           <button class="btn btn-primary">Listen</button>
+           </div>
+             
+           </div>
+         </div>
+       </div>
+         `;
+        newsContainer.appendChild(newsDiv)
+    });
+}
+
+loadAllNews();
